baa1334b8e5594ca99ea9efa5495b0b0b6fffe66
yoavhacmon <yoavh1234@gmail.com>
2022-02-14 09:18:33 +0200

    finished CompeniesDBDAO and made some changes in other

--- src/main/java/clients/dao/CompaniesDAO.java
+++ src/main/java/clients/dao/CompaniesDAO.java
@@ -2,19 +2,21 @@ package clients.dao;
 
 import clients.beans.Company;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 
 public interface CompaniesDAO {
-    public boolean isCompanyExists(String email, String password);
+    public boolean isCompanyExists(String email, String password) throws SQLException;
 
     //CRUD
-    public void addCompany(Company company);
+    public void addCompany(Company company) throws SQLException;
 
-    public void updateCompany(Company company);
+    public void updateCompany(Company company) throws SQLException;
 
-    public void deleteCompany(int  companyId);
+    public void deleteCompany(int  companyId) throws SQLException;
 
-    public ArrayList<Company> getAllCompanies();
+    public ArrayList<Company> getAllCompanies(String sql, Map<Integer, Object> values) throws SQLException;
 
-    public Company getOneCompany(int companyId);
+    public Company getOneCompany(int companyId) throws SQLException;
 }
--- src/main/java/clients/dbDao/CompaniesDBDAO.java
+++ src/main/java/clients/dbDao/CompaniesDBDAO.java
@@ -1,41 +1,114 @@
 package clients.dbDao;
 
+import clients.beans.Category;
 import clients.beans.Company;
+import clients.beans.Coupon;
 import clients.dao.CompaniesDAO;
+import clients.db.ConnectionPool;
+import clients.db.DBManager;
+import clients.db.DBTools;
 
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CompaniesDBDAO implements CompaniesDAO {
     //todo: add exceptions
     //todo: ConnectionPool connectionPool;
 
     @Override
-    public boolean isCompanyExists(String email, String password) {
-        return false;
+    public boolean isCompanyExists(String email, String password) throws SQLException {
+        Map<Integer, Object> values = new HashMap<>();
+        values.put(1, email);
+        values.put(2, password);
+        ResultSet resultSet = DBTools.runQueryForResult(DBManager.COUNT_BY_PASS_AND_EMAIL, values);
+        resultSet.next();
+        return (resultSet.getInt(1) == 1);
     }
 
     @Override
-    public void addCompany(Company company) {
+    public void addCompany(Company company) throws SQLException {
+        Map<Integer,Object> values = new HashMap<>();
+        values.put(1,company.getName());
+        values.put(2,company.getEmail());
+        values.put(3,company.getPassword());
+        DBTools.runQuery(DBManager.ADD_COMPANY,values);
+    }
+
+    @Override
+    public void updateCompany(Company company) throws SQLException {
+        Map<Integer,Object>values = new HashMap<>();
+        values.put(1,company.getName());
+        values.put(2,company.getEmail());
+        values.put(3,company.getPassword());
+        values.put(4,company.getId());
+        DBTools.runQuery(DBManager.UPDATE_COMPANY,values);
 
     }
 
     @Override
-    public void updateCompany(Company company) {
-
+    public void deleteCompany(int companyId) throws SQLException {
+        Map<Integer,Object>values = new HashMap<>();
+        values.put(1,companyId);
+        DBTools.runQuery(DBManager.DELETE_COMPANY,values);
     }
 
     @Override
-    public void deleteCompany(int companyId) {
-
+    public ArrayList<Company> getAllCompanies(String sql, Map<Integer, Object> values) throws SQLException {
+        ArrayList<Company> companies = new ArrayList<>();
+        ResultSet resultSet = DBTools.runQueryForResult(sql, values);
+        try {
+            while (resultSet.next()) {
+                ArrayList<Coupon>coupons=new ArrayList<>();
+                Company company = new Company(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        CouponsDBDAO.getCouponsByCompanyId(resultSet.getInt("id"))
+                        );
+                companies.add(company);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return companies;
     }
+
 
     @Override
-    public ArrayList<Company> getAllCompanies() {
-        return null;
+    public Company getOneCompany(int companyId) throws SQLException {
+        Company company = null;
+        Connection connection = null;
+        ArrayList<Coupon> coupons = CouponsDBDAO.getCouponsByCompanyId(companyId);
+        try{
+            connection = ConnectionPool.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(DBManager.GET_SINGLE_COMPANY);
+            preparedStatement.setInt(1,companyId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                company = new Company(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        coupons
+                );
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionPool.getInstance().restoreConnection(connection);
+        }
+        return company;
     }
 
-    @Override
-    public Company getOneCompany(int companyId) {
-        return null;
-    }
 }
