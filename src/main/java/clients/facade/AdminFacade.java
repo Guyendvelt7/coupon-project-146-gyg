28cb69d31d677d7d3ad93dac3fb3db076345b3fb
yoavhacmon <yoavh1234@gmail.com>
2022-02-22 16:21:51 +0200

    update

--- src/main/java/clients/facade/AdminFacade.java
+++ src/main/java/clients/facade/AdminFacade.java
@@ -3,7 +3,6 @@ package clients.facade;
 import clients.EnumExceptions;
 import clients.CustomExceptions;
 import clients.beans.Company;
-import clients.beans.Customer;
 import clients.dao.CompaniesDAO;
 import clients.dbDao.CompaniesDBDAO;
 
@@ -22,58 +21,36 @@ public class AdminFacade extends ClientFacade {
     }
     @Override
 
-    public boolean login(String email, String password) {
+    public boolean login(String email, String password) throws CustomExceptions {
         return this.email.equals(email) && this.password.equals(password);
 
     }
 
-    public void addCompany(Company company) throws SQLException, CustomExceptions {
+    public void addCompany(Company company) throws SQLException {
         this.companiesDBDAO.addCompany(company);
     }
 
-    public void updateCompany (Company company) throws SQLException, CustomExceptions {
+    public void updateCompany (Company company) throws SQLException {
         this.companiesDBDAO.updateCompany(company);
     }
 
-    public void deleteCompany (int companyID) throws SQLException, CustomExceptions {
+    public void deleteCompany (int companyID) throws SQLException {
         this.companiesDBDAO.deleteCompany(companyID);
         //delete coupons - cascade sql
     }
 
-    public ArrayList<Company> getAllCompanies()throws CustomExceptions{
-        ArrayList<Company> companiesList = new ArrayList<>();
-        companiesList = this.companiesDBDAO.getAllCompanies();
-        return companiesList;
+    public ArrayList<Company> getAllCompanies(){
+        return this.companiesDBDAO.getAllCompanies();
     }
 
-    public Company getOneCompany (int companyID) throws SQLException, CustomExceptions {
+    public Company getOneCompany (int companyID) throws SQLException {
         Company comp = null;
         comp = this.companiesDBDAO.getOneCompany(companyID);
         comp.setCoupons(this.companiesDBDAO.getCompanyCoupons(companyID));
         return comp;
+
     }
 
-    public void addCustomer (Customer customer){
-        this.customersDBDAO.addCustomer(customer);
-    }
-
-    public void updateCustomer(Customer customer){
-        this.customersDBDAO.updateCustomer(customer);
-    }
-
-    public void deleteCustomer(int customerID) {
-        this.customersDBDAO.deleteCustomer(customerID);
-    }
-
-    public List<Customer> getAllCustomers() {
-        List<Customer> customerList = new ArrayList<>();
-        customerList = this.customersDBDAO.getAllCustomers();
-        return customerList;
-    }
-
-    public Customer getOneCustomer(int customerID) {
-        Customer customer = null;
-        customer = this.customersDBDAO.getOneCustomer(customerID);
-        return customer;
-    }
+//    throw new CustomExceptions(EnumExceptions.INVALID_EMAIL);
+//    throw new CustomExceptions(EnumExceptions.INVALID_PASSWORD);
 }
