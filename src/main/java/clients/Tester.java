acb1ff85c79fdff1b96613fb27928431607060a0
yoavhacmon <yoavh1234@gmail.com>
2022-02-25 05:07:13 +0200

    added exceptions in CompanyFacade and in CompaniesDBDAO

---
+++ src/main/java/clients/Tester.java
@@ -0,0 +1,19 @@
+package clients;
+
+import clients.db.DBManager;
+import clients.db.DBTools;
+import clients.dbDao.CompaniesDBDAO;
+
+public class Tester {
+   public static void main(String[] args) throws CustomExceptions {
+//      DBTools.runQuery(DBManager.CREATED_DB);
+//      DBTools.runQuery(DBManager.CREATE_COMPANY_TABLE);
+//      DBTools.runQuery(DBManager.CREATE_COUPONS_TABLE);
+//      DBTools.runQuery(DBManager.CREATE_CUSTOMER_TABLE);
+//      DBTools.runQuery(DBManager.CREATE_CATEGORIES_TABLE);
+//      DBTools.runQuery(DBManager.CREATE_CUSTOMER_VS_COUPONS_TABLE);
+
+
+   }
+
+}
