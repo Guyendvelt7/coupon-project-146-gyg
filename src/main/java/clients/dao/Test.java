4f2fe9ad3e2ba879ea5a7fc97be1b37081eb01f4
Guyendvelt7 <endvelt.guy@gmail.com>
2022-02-27 15:06:04 +0200

    starting tests

---
+++ src/main/java/clients/dao/Test.java
@@ -0,0 +1,17 @@
+package clients.dao;
+
+import clients.db.DBManager;
+import clients.db.DBTools;
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        //System.out.println(DBTools.runQuery(DBManager.CREATE_COMPANY_TABLE));
+        //System.out.println(DBTools.runQuery(DBManager.CREATE_CATEGORIES_TABLE));
+        //System.out.println(DBTools.runQuery(DBManager.CREATE_COUPONS_TABLE));
+        //System.out.println(DBTools.runQuery(DBManager.CREATE_CUSTOMER_TABLE));
+        //System.out.println(DBTools.runQuery(DBManager.CREATE_CUSTOMER_VS_COUPONS_TABLE));
+
+
+    }
+}
